f425acbc8f77119fe3d2431d43f843828ea8c32c
youn6882 <youna6882@gmail.com>
2019-06-18 13:28:31 +0900

    구구단 소스 입력

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
+<!DOCTYPE html>
 <html>
-<head>Test</head>
+<head>
+    <title></title>
+</head>
 <body>
-    <h1>Helle World</h1>
+<?php
+ 
+echo "<h1>Ex. 구구단 출력하기</h1>";
+ 
+for($i=2;$i<=9;$i++){
+    echo $i."단 입니다.<br/>";
+    for($j=1;$j<=9;$j++){
+        echo $i." X ".$j." = ".($i*$j)."<br/>";
+    }
+    echo "<br/>";
+}
+ 
+?>
 </body>
 </html>
-
